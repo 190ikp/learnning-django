da55b8f8a36c658c935b27d73a38b5f87f164685
190ikp <red.volovo1996@outlook.com>
2019-04-28 17:41:32 +0900

    add detail.html and update views.py

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:post_id>/', views.detail, name='detail'),
     path('add', views.add, name='add'),
     path('delete/<int:id>', views.delete, name='delete'),
 ]
--- src/todo/views.py
+++ src/todo/views.py
@@ -5,18 +5,20 @@ from .models import Post
 from .forms import PostForm
 
 def index(request):
-    todo_list = Post.objects.all()
-    context = {
-        'todo_list': todo_list,
-    }
-    return render(request, 'todo/index.html', context)
+    todo_list = Post.objects.order_by('deadline')
+    return render(request, 'todo/index.html', {'todo_list': todo_list})
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'todo/detail.html', context)
 
 def add(request):
     form = PostForm(request.POST)
     form.save(commit=True)
     return HttpResponseRedirect(reverse('index'))
 
-def delete(req, id=None):
+def delete(request, id=None):
     post = get_object_or_404(Post, pk=id)
     post.delete()
     return HttpResponseRedirect(reverse('index'))
